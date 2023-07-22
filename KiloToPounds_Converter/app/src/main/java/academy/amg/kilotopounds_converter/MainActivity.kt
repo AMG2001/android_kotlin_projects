b8f7cbdad2542b2c3380e215cff01d75a540168a
Mohamad amgad elsaid <mohamadamgad09@gmail.com>
2023-07-22 17:55:41 +0500

    2. counter app done#

---
+++ KiloToPounds_Converter/app/src/main/java/academy/amg/kilotopounds_converter/MainActivity.kt
@@ -0,0 +1,11 @@
+package academy.amg.kilotopounds_converter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
